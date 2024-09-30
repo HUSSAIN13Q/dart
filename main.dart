d5c032df0e152ba2b2af356b47b1998f58de9307
HUSSAIN13Q <hussainalsaffar20@gmail.com>
2024-09-30 10:25:47 +0300

    In your output show at most 1 fractional digit

--- main.dart
+++ main.dart
@@ -1,8 +1,7 @@
-void main(){
-  double tempInFahrenheit = 86;
+void main() {
+  double tempInFahrenheit = 816;
   double tempInCelsius = 30;
 
-  print("$tempInFahrenheit F = ${(tempInFahrenheit-32)/1.8}C");
-  print("Celsius to Fahrenheit: = ${(tempInCelsius*1.8000)+32.00}F");
-
+  print("$tempInFahrenheit F = ${((tempInFahrenheit-32)/1.8).toStringAsFixed(1)} C");
+  print("Celsius to Fahrenheit: = ${(tempInCelsius*1.8000+32.00).toStringAsFixed(1)}F");
 }
