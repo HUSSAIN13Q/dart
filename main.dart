4f4f4ded2f753284a607fb284ffc92d3cf22dfcf
HUSSAIN13Q <hussainalsaffar20@gmail.com>
2024-09-30 09:42:09 +0300

    Dart Arithmetics

--- main.dart
+++ main.dart
@@ -2,7 +2,7 @@ void main(){
   double tempInFahrenheit = 86;
   double tempInCelsius = 30;
 
-  print("$tempInFahrenheit = ${(tempInFahrenheit-32)/1.8}C");
+  print("$tempInFahrenheit F = ${(tempInFahrenheit-32)/1.8}C");
   print("Celsius to Fahrenheit: = ${(tempInCelsius*1.8000)+32.00}F");
 
 }
